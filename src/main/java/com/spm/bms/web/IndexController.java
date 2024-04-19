cad33f7a489dae6604a2eaeadca499c55d4b50f2
gohoy <2738430398@qq.com>
2024-04-19 17:34:11 +0800

    init

---
+++ src/main/java/com/spm/bms/web/IndexController.java
@@ -0,0 +1,30 @@
+package com.spm.bms.web;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping("dev")
+    public String getDevIndex() {
+        return "index-dev.html";
+    }
+
+    @GetMapping("pro")
+    public String getProIndex(){
+        return "index.html";
+    }
+}
